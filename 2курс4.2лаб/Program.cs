4c4cf6fd691b85f403e4b71aa8040c20e6b06cf5
egorka.kamenev17@gmail.com <egorka.kamenev17@gmail.com>
2020-12-04 14:04:06 +0700

    final add minus

--- 2курс4.2лаб/Program.cs
+++ 2курс4.2лаб/Program.cs
@@ -14,6 +14,7 @@ namespace _2курс4._2лаб
 
             Console.WriteLine("Сумма: " + Convert.ToDouble(A + B));
 
+            Console.WriteLine("Разность: " + Convert.ToString(A - B));
         }
     }
 }
