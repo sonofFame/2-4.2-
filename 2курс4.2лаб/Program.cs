e205879f98754f22b388c394e2eec435676e0657
egorka.kamenev17@gmail.com <egorka.kamenev17@gmail.com>
2020-12-04 14:06:07 +0700

    final add div

--- 2курс4.2лаб/Program.cs
+++ 2курс4.2лаб/Program.cs
@@ -19,6 +19,8 @@ namespace _2курс4._2лаб
 
             Console.WriteLine("Умножение: " + Convert.ToString(A * B));
 
+            Console.WriteLine("Деление: " + Convert.ToString(A / B));
+
         }
     }
 }
